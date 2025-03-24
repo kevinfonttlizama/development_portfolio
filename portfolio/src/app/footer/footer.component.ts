38fd259e0a353c6e6a0997170d8b52a0bc828338
workliteapp <ignacio.requesens@worklite.cl>
2025-03-24 16:54:33 -0300

    cambios para vercel error en scss

--- portfolio/src/app/footer/footer.component.ts
+++ portfolio/src/app/footer/footer.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  encapsulation: ViewEncapsulation.None // Esta es la clave del cambio
 })
 export class FooterComponent {
 
--- portfolio/src/app/services/services.component.ts
+++ portfolio/src/app/services/services.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
+  encapsulation: ViewEncapsulation.None // Esta es la clave del cambio
 })
 export class ServicesComponent {
-
+  // Tu código existente
 }
